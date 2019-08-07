31dad38a3420bd704cc87bf025dc19e39a48b84f
benevpi <ben.everard@raspberrypi.org>
2019-08-07 16:41:58 +0100

    updated requirements

--- ifttt.py
+++ ifttt.py
@@ -87,18 +87,13 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
-   inline format: "* `Link Text <url>`_"
+* Should work with circuit Python boards with ESP32 wifi. 
+  Tested with a PyPortal -- https://www.adafruit.com/product/4116
 
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
-
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
