3f683a82a46407612715b8a576290945a73d0c85
benevpi <ben.everard@raspberrypi.org>
2019-08-07 14:52:28 +0100

    wrapping long lines

--- ifttt.py
+++ ifttt.py
@@ -52,6 +52,60 @@ Implementation Notes
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/benevip/CircuitPython_ifttt.git"
 
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Ben Everard for HackSpace magazine
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+`ifttt`
+================================================================================
+
+A simple link to If This Then That (IFTTT) webhooks
+
+
+* Author(s): Ben Everard
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
+   inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/benevip/CircuitPython_ifttt.git"
+
 def send_message(wifi, secrets, event, debug=False, 
                  reset_wifi_on_error=True, value1=None, 
                  value2=None, value3=None):
@@ -61,12 +115,13 @@ def send_message(wifi, secrets, event, debug=False,
         return
     while not sent:
         try:
-            if debug: print("Posting data...", end='')
+            if debug: print("Posting data")
             payload = {}
             if value1 is not None: payload['value1'] = value1
             if value2 is not None: payload['value2'] = value2
             if value3 is not None: payload['value3'] = value3
-            url = "https://maker.ifttt.com/trigger/" + event + "/with/key/"+secrets['ifttt_key']
+            url = "https://maker.ifttt.com/trigger/" + event + "/with/key/" + \
+                secrets['ifttt_key']
             if debug: print(url)
             response = wifi.post(url,json=payload)
             response.close()
@@ -77,5 +132,5 @@ def send_message(wifi, secrets, event, debug=False,
             if reset_wifi_on_error:
                 wifi.reset()
             else:
-                sent = True
+                sent=True
 
